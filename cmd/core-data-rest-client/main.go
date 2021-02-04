c7835b740126bacdaeb3cd9aab3c2b94be99d8c7
chao zheng <charleszheng44@gmail.com>
2021-02-03 19:45:17 -0800

    add json decoding

--- cmd/core-data-rest-client/main.go
+++ cmd/core-data-rest-client/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+
+	"github.com/edgexfoundry/go-mod-core-contracts/models"
 	"github.com/go-resty/resty/v2"
-	"path/filepath"
 )
 
 type CoreServiceClientConfig struct {
@@ -16,25 +18,21 @@ func main() {
 
 	host := flag.String("host", "localhost", "TODO...")
 	port := flag.Int("port", 48080, "TODO...")
-	path := flag.String("path", "", "TODO...")
 
 	flag.Parse()
 
-	restPath := fmt.Sprintf("http://%s:%d%s", host, port, path)
+	restPath := fmt.Sprintf("http://%s:%d/api/v1/valuedescriptor", *host, *port)
 
 	client := resty.New()
 	resp, err := client.R().
 		EnableTrace().
 		Get(restPath)
-
-	// Explore response object
-	fmt.Println("Response Info:")
-	fmt.Println("  Error      :", err)
-	fmt.Println("  Status Code:", resp.StatusCode())
-	fmt.Println("  Status     :", resp.Status())
-	fmt.Println("  Proto      :", resp.Proto())
-	fmt.Println("  Time       :", resp.Time())
-	fmt.Println("  Received At:", resp.ReceivedAt())
-	fmt.Println("  Body       :\n", resp)
-	fmt.Println()
+	if err != nil {
+		panic(err)
+	}
+	vds := []models.ValueDescriptor{}
+	if json.Unmarshal(resp.Body(), &vds); err != nil {
+		panic(err)
+	}
+	fmt.Println(vds)
 }
--- go.mod
+++ go.mod
@@ -3,15 +3,12 @@ module github.com/charleszheng44/edgex-sample-clients
 go 1.15
 
 require (
-	github.com/edgexfoundry/consul-client-go v0.0.0-20171213112924-1791ee60ca82 // indirect
-	github.com/edgexfoundry/core-clients-go v0.0.0-20180301031049-a98760b6bde0 // indirect
-	github.com/edgexfoundry/core-data-go v0.0.0-20180105155421-58619b9a0201 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/edgexfoundry/core-data-go v0.0.0-20180105155421-58619b9a0201
 	github.com/edgexfoundry/core-domain-go v0.0.0-20180130224812-7acdb6490aba // indirect
-	github.com/edgexfoundry/support-domain-go v0.0.0-20171226130833-c5112cbbc50e // indirect
-	github.com/edgexfoundry/support-logging-client-go v0.0.0-20180104221006-6048b3cf4525 // indirect
+	github.com/edgexfoundry/go-mod-core-contracts v0.1.111
 	github.com/go-resty/resty/v2 v2.4.0
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/hashicorp/consul/api v1.8.1 // indirect
-	github.com/pebbe/zmq4 v1.2.2 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
